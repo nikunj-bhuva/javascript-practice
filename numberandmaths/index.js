2b9a20a7d156c19f47dc99a9c115d45fec7a4f96
nikunj-bhuva <nikunjbhuva121@gmail.com>
2024-10-16 03:06:59 +0530

    javascript practice

---
+++ numberandmaths/index.js
@@ -0,0 +1,23 @@
+// let balance = new Number(200);
+// console.log(balance);
+// console.log(typeof balance.toString());
+
+// tofixed tamne number na points pachi ketli value joi chi te tema number of digit pass karvana hoy che //
+
+// console.log(balance.toFixed(2));
+
+// let hundreds = 100000;
+// console.log(hundreds.toLocaleString("en-IN")); //by default tolocalstring us standard ma number ape che pn tamare indian standard ma jotu hoy to 'en-IN' string thi pass karvanu function etle indian standard ma value madse.
+
+// console.log(Math.abs(-5)); //abs method negative value ne positive karva mate kam ave che.
+
+// console.log(Math.floor(6.9));//floor method hamesha decimal ma j digit hase tena thi niche jase ane te integer j apse means k decimal number thi nani value apse negative number ma pn same j rese.
+
+// console.log(Math.round(5.5)); // round method jo decimal ma 0.5 thi uper hoy to te je integer hein tena pchi no number apse ane jo 0.4 hase k tena thi niche hase to je integer hase tene te number j round kari dese.
+
+// console.log(Math.ceil(1.1));// ceil method hamesh je decimal value apso positive ma k negative ma te decimal thi moti poora number j hase e return karse jem k 4.1 to 4.1 thi moto pooro number 5 che to te 5 return karse.
+// console.log(Math.ceil(-5.9));
+
+// random method hamesha 0 thi 1 thi vache no number return karse
+
+// console.log(Math.round(Math.random() * 10));
