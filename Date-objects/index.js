94d920375d7910dc8e008d213e1f99fbd9ca989b
nikunj-bhuva <nikunjbhuva121@gmail.com>
2024-10-16 02:13:47 +0530

    javascript practice

--- Date-objects/index.js
+++ Date-objects/index.js
@@ -108,26 +108,150 @@
 
 // let date = new Date("2024-7-21");
 // console.log(date);
-// for (let i = 0; i < 10000; i++) {
-//   console.log(i);
-// }
 
-// for (let i = 1; i < 10; i++) {
-//   console.log(i);
-// }
+// practice 15/10/202
 
-// // debugger;
-// let val1 = 10;
-// let val2 = 5;
+// const date = new Date();
+// console.log(date);
 
-// // debugger;
-// function addNum(num1, num2) {
-//   // debugger;
-//   let result = num1 + num2;
-//   return result;
-// }
-// // debugger;
-// let result1 = addNum(val1, val2);
-// console.log(result1);
-// let result2 = addNum(10, 8);
+// create date object with specified year and month.
 
+// let date = new Date(2024, 10);
+// console.log(date);
+
+// create date object with specified year month and date.
+
+// let date = new Date(2024, 9, 15);
+// console.log(date);
+
+// create date object with specified year month and date hours.
+
+// let date = new Date(2024, 9, 15, 21);
+// console.log(date);
+
+// create date object with specified year month and date hours minutes.
+
+// let date = new Date(2024, 9, 15, 9, 26);
+// console.log(date);
+
+// create date object with specified year month and date hours minutes seconds.
+
+// let date = new Date(2024, 9, 15, 9, 28, 45);
+// console.log(date);
+
+// create date object with specified year month and date hours minutes seconds miliseconds.
+
+// let date = new Date(2024, 9, 15, 1, 20, 55, 200);
+// console.log(date);
+
+// get time using miliseconds //
+
+// let date = new Date().getTime();
+// let getTime = new Date(date);
+// console.log(getTime);
+
+// date get methods into the date object //
+
+// get year //
+
+// let date = new Date();
+// let getYear = date.getFullYear();
+// console.log(getYear);
+
+// get month //
+
+// let getMonth = date.getMonth();
+// console.log(getMonth);
+
+// get date //
+
+// let getDate = date.getDate();
+// console.log(getDate);
+
+// get hours //
+
+// let getHours = date.getHours();
+// console.log(getHours);
+
+// get minutes //
+
+// let getMinutes = date.getMinutes();
+// console.log(getMinutes);
+
+// get seconds //
+
+// let getSeconds = date.getSeconds();
+// console.log(getSeconds);
+
+// get miliseconds //
+
+// let getMiliSeconds = date.getMilliseconds();
+// console.log(getMiliSeconds);
+
+// get day //
+
+// day stats from index number 0 means sunday=0 //
+
+// let getDay = date.getDay();
+// console.log(getDay);
+
+// js set method into the date objects //
+
+// set year //
+
+// date.setFullYear(2025);
+
+// set month //
+
+// date.setMonth(9);
+
+//set date //
+
+// date.setDate(16);
+
+// set hours //
+
+// date.setHours(1);
+
+// set minutes //
+
+// date.setMinutes(46);
+
+// set seconds //
+
+// date.setSeconds(40);
+
+// set month
+
+// date.setMonth(3);
+// console.log(date);
+
+// tolocalstring method //
+
+// it give you time and date to your local system time aur date //
+
+// let localString = date.toLocaleString();
+// console.log(localString);
+
+// you want only date using tolocaldatestring method //
+
+// let localDateString = date.toLocaleDateString();
+// console.log(localDateString);
+
+// you want only time using tolocaltimestring method //
+
+// let localTimeString = date.toLocaleTimeString();
+// console.log(localTimeString);
+
+// write a function to add a specified number of days to a given date //
+
+// let date = new Date("2024-10-16");
+
+// const addDaysToDate = (date, extraDay) =>
+//   new Date(date.setDate(date.getDate() + extraDay));
+
+// const newDate = addDaysToDate(date, 7);
+// console.log(newDate);
+// console.log(newDate.toLocaleDateString());
+
+// write a function  to calculate the difference in days between two given dates //
