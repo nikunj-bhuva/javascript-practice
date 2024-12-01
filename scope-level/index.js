98ff24948f77fb5e68c6521a91ee325db1d80a09
nikunj-bhuva <nikunjbhuva121@gmail.com>
2024-12-01 20:10:20 +0530

    javascript practice

--- scope-level/index.js
+++ scope-level/index.js
@@ -37,3 +37,55 @@
 // let addTwo = function (num1, num2) {
 //   return num1 + num2;
 // };
+
+// practice 1-12-2024 //
+
+// let a = 300;
+
+// if (true) {
+//   let a = 10;
+//   const b = 20;
+//   var c = 30;
+// }
+// // console.log(a);//not defined error avse karan k let block scope variable che te tena block n andar j tame tene access kari sako che //
+// // console.log(b);//same as let keyword //
+// console.log(c); //access kari sakso karan k var che e global scope variable che te gme tayathi access kri sako tame tene
+// console.log(a);
+
+// function parent() {
+//   let fatherName = "jacky shroff";
+//   function child() {
+//     let childName = "tiger shroff";
+//     console.log(fatherName + " " + childName); // child function ni andar parent function na variable access kari sako //
+//   }
+//   // console.log("father name:", fatherName); //father name access thase karan k te parent function na block ma j che //
+//   // console.log("child name:", childName); //child Name access nai thay karan k js parent function na block ma childName no variable find karan te tame taya declare nathi karyo etle refrence error avse not defined karan k te child function na block ma declare karyo che //
+//   child();
+// }
+
+// parent();
+
+// if (true) {
+//   const userName = "nikunj";
+//   if (userName === "nikunj") {
+//     const website = "insta";
+//     console.log(userName + " " + website);
+//   }
+//   // console.log(website);
+// }
+
+// console.log(userName);
+
+// console.log(addOne(10)); //access kari sakso //
+
+// function addOne(num) {
+//   return num + 1;
+// }
+
+// // function expression //
+
+// addTwo(20); // access nahi karo sako error avse karan k tame function n variable ni andar store karyu che to variable tme niche declare karyo che tene pehla tame tenu access na kari sako //
+
+// let addTwo = function (num) {
+//   return num + 2;
+// };
